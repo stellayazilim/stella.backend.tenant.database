efb6389b16664a30fef715162a7a9b22437b7085
KerimCetinbas <a.kerim.cetinbas@yandex.com>
2023-08-08 18:19:45 +0300

    add column public.products.brand

--- tables/product.table.sql
+++ tables/product.table.sql
@@ -1,6 +1,11 @@
-CREATE TABLE IF NOT EXISTS  products  (
-    id SMALLSERIAL PRIMARY KEY NOT NULL,
-    name VARCHAR(32) UNIQUE NOT NULL ,
-    description VARCHAR(124),
-    explanation TEXT
-)
+CREATE TABLE IF NOT EXISTS public.products  (
+    "id" SMALLSERIAL PRIMARY KEY NOT NULL,
+    "name" VARCHAR(32) UNIQUE NOT NULL ,
+    "description" VARCHAR(124),
+    "explanation" TEXT
+);
+
+
+ALTER TABLE IF EXISTS
+    public.products
+    ADD COLUMN "brand" VARCHAR(56)  DEFAULT "Brand" NOT NULL;
